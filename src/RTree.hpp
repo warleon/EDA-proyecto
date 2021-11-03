750b27e50e3fab7d385d8d99406b645660d32958
warleon <51892390+warleon@users.noreply.github.com>
2021-11-03 00:29:15 -0500

    something

---
+++ src/RTree.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class RTree {
+ public:
+  RTree() {}
+  ~RTree() {}
+};
---
+++ tests/RTreeTest.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+#include <RTree.hpp>
+
+// Demonstrate some basic assertions.
+TEST(RTreeTest, test1) { RTree testTree; }
